f5d14c1b6d4e0ea9b45e0bc9ece4c6fb1765bc1b
Ayush Kumar <165643930+ayush9718@users.noreply.github.com>
2026-07-01 16:38:04 +0530

    Time: 13 ms (59.95%) | Memory: 23.9 MB (16.94%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int n;
     // using bitmasking for visited because nums.length is small
+    vector<int> dp;
     bool solve(vector<int>& nums, int mask, int index, int currsum, int target, int k){
         if(k == 1){
             return true;
         }
+        if(dp[mask]!=-1){
+            return dp[mask];
+        }
 
         if(currsum == target){
-            return solve(nums,mask,0,0,target,k-1);
+            return dp[mask] = solve(nums,mask,0,0,target,k-1);
         }
 
         for(int i =index; i<n; i++){
@@ -22,12 +26,12 @@ public:
             bool recans = solve(nums, mask|(1<<i) ,i+1,currsum+nums[i],target,k);
 
             if(recans == true){
-                return true;
+                return dp[mask] = true;
             }
 
         }
 
-        return false;
+        return dp[mask] =  false;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         n = nums.size();
@@ -38,6 +42,7 @@ public:
         if(sum%k != 0){
             return false ;
         }
+        dp.assign(1<<nums.size(), -1);
 
         return solve(nums,0,0,0,sum/k,k);
     }
