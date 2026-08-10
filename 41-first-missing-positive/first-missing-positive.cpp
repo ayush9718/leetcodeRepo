1d7794ca0afd307870858190c88d2f48dbde7c4d
Ayush Kumar <165643930+ayush9718@users.noreply.github.com>
2026-08-11 02:06:46 +0530

    Time: 785 ms (5.01%) | Memory: 54.9 MB (86.28%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,22 +2,40 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-
-        unordered_map<int,bool> mp;
-
-        for(int i=0; i<n; i++){
-            if(nums[i]<=0){
-                continue ;
+        bool present_one = false;
+        for(int i =0; i<n;i++){
+            if(nums[i] == 1){
+                present_one = true;
             }
-            mp[nums[i]] = true;
-        }
-
-        for(int i =1; i<=n+1; i++){
-            if(mp[i] == false){
-                return i;
+             if(nums[i] >n || nums[i]<=0){
+                nums[i] = 1;
             }
         }
+        if(present_one == false){
+            return 1;
+        }
 
-        return 1;
+
+        for(int i =0; i<n; i++){
+            int curr = abs(nums[i]);
+            int index = curr-1;
+
+            if(nums[index] >0){
+                nums[index] = nums[index]*-1;
+            }
+        }
+
+        int ans = n+1;
+        for(int i =0;i<n; i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+        for(int i =0; i<n; i++){
+            int curr = nums[i];
+            if(curr >0){
+                return  i+1;
+            }
+        }
+        return ans;
     }
 };
