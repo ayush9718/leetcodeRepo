25158915b33b590f9f6e4267bc068b724bcd6cb3
Ayush Kumar <165643930+ayush9718@users.noreply.github.com>
2026-08-22 16:40:57 +0530

    Time: 10 ms (7.98%) | Memory: 16.6 MB (6.24%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*,TreeNode*> parent;
+    void dfs(TreeNode* root, TreeNode* prnt){
+        if(root == NULL){
+            return ;
+        }
+        parent[root] = prnt;
+        
+        dfs(root->left,root);    
+        dfs(root->right,root);
+    }
+    unordered_map<TreeNode*, bool> visited;
+    vector<int> ans;
+
+    void dfs2(TreeNode* root, int k){
+        if(root == NULL || visited[root] == true){
+            return ;
+        }
+        if(k == 0 && root!= NULL){
+            ans.push_back(root->val);
+            return ;
+        }
+
+        visited[root] = true;
+
+       
+        dfs2(root->left, k-1);
+        dfs2(root->right,k-1);
+        dfs2(parent[root],k-1);
+
+        return ;
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        dfs(root,NULL);
+
+        // now i have parent of every node
+        // now apply dfs again and do something like burning tree
+        dfs2(target,k);
+
+        return ans;
+    }
+};
