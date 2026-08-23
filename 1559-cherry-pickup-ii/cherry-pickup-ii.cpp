1a8df525b2e76a6f29a9649be8c12311a7b44287
Ayush Kumar <165643930+ayush9718@users.noreply.github.com>
2026-08-23 19:52:00 +0530

    Time: 37 ms (83.86%) | Memory: 21.7 MB (7.04%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -14,8 +14,8 @@ public:
         if(dp[row][c1][c2] != -1){
             return dp[row][c1][c2];
         }
-        vector<int> dx = {1,1,1};
-        vector<int> dy = {-1,0,1};
+        // vector<int> dx = {1,1,1};
+        // vector<int> dy = {-1,0,1};
         
         int ans = INT_MIN;
         int temp = 0;
@@ -27,13 +27,23 @@ public:
         }
 
 
-        for(int i =0; i<3; i++){
-            for(int j =0; j<3; j++){
-                int newrow = row+dx[i];
-                int newc1 = c1+dy[i];
-                int newc2 = c2+dy[j];
+        // for(int i =0; i<3; i++){
+        //     for(int j =0; j<3; j++){
+        //         int newrow = row+dx[i];
+        //         int newc1 = c1+dy[i];
+        //         int newc2 = c2+dy[j];
 
-               ans = max(ans, temp + solve(newrow,newc1,newc2));
+        //        ans = max(ans, temp + solve(newrow,newc1,newc2));
+        //     }
+        // }
+
+        for (int d1 = -1; d1 <= 1; d1++) {
+            for (int d2 = -1; d2 <= 1; d2++) {
+
+                int nextC1 = c1 + d1;
+                int nextC2 = c2 + d2;
+
+                ans = max(ans,temp + solve(row + 1, nextC1, nextC2));
             }
         }
 
